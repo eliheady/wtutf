cb25c03c436b11c4ad528019e1f1fa2310a8a84f
Eli Heady <eliheady@pm.me>
2025-03-25 10:27:00 -0400

    [wip] improve error reporting per character

--- Readme.md
+++ Readme.md
@@ -2,40 +2,43 @@
 
 A simple utility to help me out of my ASCII-centric shell
 
-This program just prints out the Unicode code points of the string you feed into it. It can also show you the punycode conversion.
+This program just prints out the Unicode code points of the string you feed into it. It can also show you the punycode conversion of your string, or failure reasons if conversion isn't possible.
 
 ```shell
-$ wtutf piñata
-total bytes: 8
-characters: 7
-    code point  (bytes)
-p:      0x70    (1)
-i:      0x69    (1)
-n:      0x6e    (1)
-̃:      0x303   (2)
-a:      0x61    (1)
-t:      0x74    (1)
-a:      0x61    (1)
-punycode:
-could not punycode-convert input
+$ wtutf -s "piñata"
+total bytes:    8
+characters:     7
+punycode:       could not punycode-convert input
+    code point | bytes  | conversion errors
+p:        0x70 | (1)    | 
+i:        0x69 | (1)    | 
+n:        0x6e | (1)    | 
+̃:      0x0303 | (2)    | CheckJoiners (RFC 5892), ValidateForRegistration (RFC 5891), ValidateLabels (RFC 5891), UseSTD3ASCIIRules (RFC 1034, 5891, UTS 46)
+a:        0x61 | (1)    | 
+t:        0x74 | (1)    | 
+a:        0x61 | (1)    |  
 
-$ wtutf piñata  
-total bytes: 7
-characters: 6
-    code point  (bytes)
-p:      0x70    (1)
-i:      0x69    (1)
-ñ:      0xf1    (2)
-a:      0x61    (1)
-t:      0x74    (1)
-a:      0x61    (1)
-punycode:
-xn--piata-pta
-.
-
-Usage:
-  wtutf [flags]
-
-Flags:
-  -h, --help   help for wtutf
+$ wtutf -s "piñata"
+total bytes:    7
+characters:     6
+punycode:       xn--piata-pta
+    code point | bytes 
+p:        0x70 | (1)
+i:        0x69 | (1)
+ñ:        0xf1 | (2)
+a:        0x61 | (1)
+t:        0x74 | (1)
+a:        0x61 | (1)
 ```
+
+### Why make this?
+
+I was interested in punycode and IDNA standards, and wanted to make a simple utility to run locally to test coversion of various Unicode characters.
+
+The Unicode Transformation Format – 8-bit (UTF-8) encoding allows for some difficult to interpret strings even when your rendering environment doesn't garble the characters with question marks or boxes.
+
+The combining characters are a good example. The usage string above shows two strings that look identical on my system: "piñata" and "piñata". Only if I examine the bytes of those strings can I see that the second one uses 0x6e (n) and the UTF-8 "combining tilde" character 0x0303 ( ̃) to create the Spanish eñe. The first uses the single 0xf1 (ñ) "precomposed character".
+
+The combining characters aren't allowed in IDN domain registrations because they would provide a way to register names that are visually indistinguishable, making things confusing for online piñata shopping.
+
+This program helps see what went into the strings that look identical but aren't. It is also useful if you need to troubleshoot punycode conversion.
--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 	"unicode/utf8"
 
 	"github.com/spf13/cobra"
@@ -16,37 +17,9 @@ var rootCmd = &cobra.Command{
 	Use:   "wtutf",
 	Args:  cobra.ExactArgs(1),
 	Short: "A simple utility to help me out of my ASCII-centric shell",
-	Long: `This program just prints out the Unicode code points of the string you feed into it. It can also show you the punycode conversion.
-
-$ wtutf piñata
-total bytes: 8
-characters: 7
-    code point  (bytes)
-p:      0x70    (1)
-i:      0x69    (1)
-n:      0x6e    (1)
-̃:      0x303   (2)
-a:      0x61    (1)
-t:      0x74    (1)
-a:      0x61    (1)
-punycode:
-could not punycode-convert input
-
-$ wtutf piñata  
-total bytes: 7
-characters: 6
-    code point  (bytes)
-p:      0x70    (1)
-i:      0x69    (1)
-ñ:      0xf1    (2)
-a:      0x61    (1)
-t:      0x74    (1)
-a:      0x61    (1)
-punycode:
-xn--piata-pta
-.`,
+	Long:  `This program just prints out the Unicode code points of the string you feed into it. It can also show you the punycode conversion of your string, or failure reasons if conversion isn't possible.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print(processInput(args[0]))
+		fmt.Print(processInput(cmd, args[0]))
 	},
 }
 
@@ -57,35 +30,107 @@ func Execute() {
 	}
 }
 
-func processInput(ustring string) string {
+func init() {
+	var strict bool
+	rootCmd.PersistentFlags().BoolVarP(&strict, "strict", "s", false, "Set strict punycode conversion rules")
+}
 
-	if ustring == "" {
-		fmt.Println("uh, sure ... the empty string is")
+// toPuny takes a rune and an idna.Option rule and calls idna.ToASCII,
+// returning the punycode string and error
+func toPuny(s string, rules []idna.Option) (string, error) {
+	punyRules := idna.New(
+		rules...,
+	)
+	return punyRules.ToASCII(s)
+}
+
+// canPunyConvert takes a rune and an idna.Option rule and attempts the ToASCII
+// conversion, then reports success
+func canPunyConvert(s string, rules []idna.Option) bool {
+	_, err := toPuny(s, rules)
+	return err == nil
+}
+
+// enumerateErrors takes a rune, checks several punycode conversion rules and
+// reports the failures as a single string
+func enumerateErrors(r rune) string {
+	rules := map[string][]idna.Option{
+		"CheckBidi (RFC 5893)":                       {idna.BidiRule()},
+		"CheckJoiners (RFC 5892)":                    {idna.CheckJoiners(true)},
+		"CheckHyphens (UTS 46)":                      {idna.CheckHyphens(true)},
+		"ValidateForRegistration (RFC 5891)":         {idna.ValidateForRegistration()},
+		"ValidateLabels (RFC 5891)":                  {idna.ValidateLabels(true)},
+		"UseSTD3ASCIIRules (RFC 1034, 5891, UTS 46)": {idna.StrictDomainName(true), idna.ValidateLabels(true)},
 	}
 
+	var allErrors []string
+
+	for i, ruleset := range rules {
+		if !canPunyConvert(string(r), ruleset) {
+			allErrors = append(allErrors, i)
+		}
+	}
+
+	return strings.Join(allErrors, ", ")
+
+}
+
+func processInput(cmd *cobra.Command, ustring string) string {
+
+	flags := cmd.Flags()
 	var out string
 
-	out += fmt.Sprintf("total bytes: %d\n", len(ustring))
-	out += fmt.Sprintf("characters: %d\n", utf8.RuneCountInString(ustring))
+	out += fmt.Sprintf("total bytes:\t%d\n", len(ustring))
+	out += fmt.Sprintf("characters:\t%d\n", utf8.RuneCountInString(ustring))
 
-	out += "    code point\t(bytes)\n"
-	for _, r := range ustring {
-		out += fmt.Sprintf("%c:\t%#02x\t(%d)\n", r, r, len(string(r)))
-	}
-
-	puny := idna.New(
+	rules := []idna.Option{
 		idna.BidiRule(),
 		idna.CheckJoiners(true),
-		idna.CheckHyphens(true),
-		idna.ValidateForRegistration(),
 		idna.ValidateLabels(true),
-	)
-	out += "punycode:\n"
-	if pc, err := puny.ToASCII(ustring); err == nil {
-		out += pc
+	}
+	if strict, _ := flags.GetBool("strict"); strict { // ok to discard err if flag was not set
+		rules = append(rules,
+			idna.ValidateForRegistration(),
+			idna.StrictDomainName(true),
+		)
+	}
+
+	var converted bool
+	out += "punycode:\t"
+	if punycode, err := toPuny(ustring, rules); err == nil {
+		converted = true
+		out += punycode
 	} else {
 		out += "could not punycode-convert input"
 	}
 	out += "\n"
+
+	header := "    code point | bytes "
+	if !converted {
+		header += " | conversion errors"
+	}
+	out += header + "\n"
+
+	for _, r := range ustring {
+		safeToPrint := r
+		moreErrors := ""
+		var padded string
+		switch {
+		case r < 0x20:
+			safeToPrint = ' '
+			padded = fmt.Sprintf("%#02x", r)
+		case r > 0xff:
+			padded = fmt.Sprintf("%#04x", r)
+		case r > 0xffff:
+			padded = fmt.Sprintf("%#06x", r)
+		default:
+			padded = fmt.Sprintf("%#02x", r)
+		}
+		if !converted {
+			moreErrors = fmt.Sprintf("\t| %s", enumerateErrors(r))
+		}
+		out += fmt.Sprintf("%c:\t% 6s | (%d)%s\n", safeToPrint, padded, len(string(r)), moreErrors)
+	}
+
 	return out
 }
